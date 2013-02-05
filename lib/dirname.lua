a8f55bd2579a4d7aa8a6f13652d46c0b8a06b4bd
Gianluca Fiore <forod.g@gmail.com>
2013-02-05 11:42:11 +0100

    improve dirname.lua by returning an empty string when no directory path found

--- lib/dirname.lua
+++ lib/dirname.lua
@@ -3,6 +3,10 @@
 --- Function equivalent to dirname in POSIX systems
 --@param str the path string
 function dirname(str)
-	local name = string.gsub(str, "(.*/)(.*)", "%1")
-	return name
+	if str:match(".-/.-") then
+		local name = string.gsub(str, "(.*/)(.*)", "%1")
+		return name
+	else
+		return ''
+	end
 end
