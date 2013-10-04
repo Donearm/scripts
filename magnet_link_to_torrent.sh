082998ea9fdb974291996d0db158d8f9297ad98d
Gianluca Fiore <forod.g@gmail.com>
2013-10-04 09:38:54 +0200

    send magnet links to rTorrent watch directories

--- magnet_link_to_torrent.sh
+++ magnet_link_to_torrent.sh
@@ -2,7 +2,25 @@
 
 # echo a magnet link to a torrent file
 
-cd /media/private/torrent/
+WATCHDIR='/media/private/torrent/watch' # set your watch directory here
 
-[[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit;
-echo "d10:magnet-uri${#1}:${1}e" > "meta-${BASH_REMATCH[1]}.torrent"
+cd $WATCHDIR || exit
+[[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit
+hashh=${BASH_REMATCH[1]}
+if [[ "$1" =~ dn=([^&/]+) ]];then
+  filename=${BASH_REMATCH[1]}
+else
+  filename=$hashh
+fi
+
+if [[ "$2" == 'vids' ]]; then
+	directory="${WATCHDIR}/vids/"
+elif [[ "$2" == 'books' ]]; then
+	directory="${WATCHDIR}/books/"
+elif [[ "$2" == 'music' ]]; then
+	directory="${WATCHDIR}/music/"
+else
+	directory="${WATCHDIR}/others/"
+fi
+
+echo "d10:magnet-uri${#1}:${1}e" > "${directory}meta-$filename.torrent"
