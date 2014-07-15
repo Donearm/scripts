a02726297ba935fee9e6f83f919952ba121a7b6f
Gianluca Fiore <forod.g@gmail.com>
2014-07-15 10:31:17 +0200

    Add helper functions to read/write files in Go

---
+++ lib/read_write_files.go
@@ -0,0 +1,33 @@
+import (
+	"bufio"
+	"os"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, Scanner.Err()
+}
+
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Println(w, line)
+	}
+	return w.Flush()
+}
