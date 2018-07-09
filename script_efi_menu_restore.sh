a4fb4df02710a24f53b54afdb2acc0b662dc166b
Gianluca Fiore <forod.g@gmail.com>
2018-07-09 10:48:24 +0200

    set dev variable for efi device

--- script_efi_menu_restore.sh
+++ script_efi_menu_restore.sh
@@ -4,9 +4,10 @@
 # that I care, of course
 
 EFIBOOTMGR=$(which efibootmgr)
+DEV="/dev/sda"
 
-$EFIBOOTMGR -c -g -d /dev/sda -p 1 -w -L "Windows8" -l '\EFI\Microsoft\Boot\bootmgfw.efi'
-$EFIBOOTMGR -c -g -d /dev/sda -p 1 -w -L "rEFInd" -l '\EFI\refind\refind_x64.efi'
-$EFIBOOTMGR -c -g -d /dev/sda -p 1 -w -L "Grub" -l '\EFI\arch_grub\grubx64_standalone.efi'
+$EFIBOOTMGR -c -g -d $DEV -p 1 -w -L "Windows8" -l '\EFI\Microsoft\Boot\bootmgfw.efi'
+$EFIBOOTMGR -c -g -d $DEV -p 1 -w -L "rEFInd" -l '\EFI\refind\refind_x64.efi'
+$EFIBOOTMGR -c -g -d $DEV -p 1 -w -L "Grub" -l '\EFI\arch_grub\grubx64_standalone.efi'
 
 exit 0
