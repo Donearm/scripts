0bf38171a2929289583125a18f8483e47e5ea482
Gianluca Fiore <forod.g@gmail.com>
2018-11-28 18:33:04 +0100

    extrapolating session name from work directory in tmux_workon_project.sh

--- tmux_workon_project.sh
+++ tmux_workon_project.sh
@@ -4,23 +4,26 @@
 # Needs to be run like:
 # script <name of the project> <path where the project resides>
 
-if [ $# -lt 2 ];
+if [ $# -lt 1 ];
 then
 	echo "Usage: "
-	echo -e "\ttmux_workon_project.sh <project name> <project directory>"
+	echo -e "\ttmux_workon_project.sh <project directory>"
 	exit 1
 fi
 
+TARGET_DIR="$@"
+TARGET_NAME=$(basename "$TARGET_DIR")
+
 # Start a new session
-tmux new-session -d -n "$1" -s "$1" /bin/bash
+tmux new-session -d -s "$TARGET_NAME" -n "$TARGET_DIR" /bin/bash
 
 # cd into project's path (main pane)
-tmux send-keys "cd $2" C-m
+tmux send-keys "cd $TARGET_DIR" C-m
 
 # Split vertically
-tmux split-window -h -c "$2"
+tmux split-window -h -c "$TARGET_DIR"
 
 # Attach the new session
-tmux attach-session -t "$1"
+tmux attach-session -t "$TARGET_NAME"
 
 exit 0
