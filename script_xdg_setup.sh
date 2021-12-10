9bae4fe9eb16559b1e25e8517ec1bb92809d8b3d
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2021-12-10 16:39:32 +0100

    updated programs and some mimetypes in script_xdg_setup.sh

--- script_xdg_setup.sh
+++ script_xdg_setup.sh
@@ -2,10 +2,10 @@
 #
 # Raw and quick script to set up the file's association for xdg-open
 
-IMAGEVIEWER="imv.desktop"
+IMAGEVIEWER="feh.desktop"
 VIDEOPLAYER="mpv.desktop"
-EBOOKREADER="com.github.johnfactotum.Foliate.desktop"
-DOCREADER="apvlv.desktop"
+EBOOKREADER="org.pwmt.zathura.desktop"
+DOCREADER="org.pwmt.zathura.desktop"
 
 xdg-mime default "${BROWSER}.desktop" text/html
 xdg-mime default "${BROWSER}.desktop" application/x-extension-xht
@@ -18,15 +18,22 @@ xdg-mime default $IMAGEVIEWER image/jpeg
 xdg-mime default $IMAGEVIEWER image/pjpeg
 xdg-mime default $IMAGEVIEWER image/png
 xdg-mime default $IMAGEVIEWER image/gif
+xdg-mime default $IMAGEVIEWER image/webp
 xdg-mime default $IMAGEVIEWER image/*
 
 xdg-mime default $VIDEOPLAYER video/mp4
 xdg-mime default $VIDEOPLAYER video/mkv
 xdg-mime default $VIDEOPLAYER video/mpeg
+xdg-mime default $VIDEOPLAYER video/avi
+xdg-mime default $VIDEOPLAYER video/x-msvideo
 xdg-mime default $VIDEOPLAYER video/*
 
+xdg-mime default $VIDEOPLAYER application/ogg
 xdg-mime default $VIDEOPLAYER audio/*
 
 xdg-mime default $EBOOKREADER application/epub+zip
 
 xdg-mime default $DOCREADER application/pdf
+xdg-mime default $DOCREADER  application/postscript
+# Djvu files open with Zathura (needs zathura-djvu package)
+xdg-mime default $DOCREADER image/vnd.djvu
