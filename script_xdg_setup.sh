ddf6d8a1601cd4d359f3874a707f6d4974049b4a
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2022-03-03 19:42:55 +0100

    reorganized script_xdg_setup.sh

--- script_xdg_setup.sh
+++ script_xdg_setup.sh
@@ -6,35 +6,66 @@ IMAGEVIEWER="feh.desktop"
 VIDEOPLAYER="mpv.desktop"
 EBOOKREADER="org.pwmt.zathura.desktop"
 DOCREADER="org.pwmt.zathura.desktop"
+TEXTREADER="vim.desktop"
 
-xdg-mime default "${BROWSER}.desktop" text/html
-xdg-mime default "${BROWSER}.desktop" application/x-extension-xht
-xdg-mime default "${BROWSER}.desktop" application/x-extension-xhtml
-xdg-mime default "${BROWSER}.desktop" application/xhtml+xml
-xdg-mime default "${BROWSER}.desktop" application/x-extension-htm
-xdg-mime default "${BROWSER}.desktop" application/x-extension-html
+# Setting up a few arrays depending on the application we need
+# mimetypes to be associated with
+BROWSER_ARRAY=('text/html',
+	'application/x-extension-xht',
+	'application/x-extension-xhtml',
+	'application/xhtml+xml',
+	'application/x-extension-htm',
+	'application/x-extension-html'
+)
+IMAGES_ARRAY=('image/jpeg',
+	'image/pjpeg',
+	'image/png',
+	'image/gif/',
+	'image/webp',
+	'image/*'
+)
+VIDEOS_ARRAY=('video/mp4',
+	'video/mkv',
+	'video/mpeg',
+	'video/x-msvideo',
+	'video/webm',
+	'video/*',
+	'application/ogg',
+	'audio/*'
+)
+DOCUMENTS_ARRAY=('application/pdf',
+	'application/postscript',
+# Djvu files open with Zathura (needs zathura-djvu package)
+	'image/vnd.djvu',
+	'application/vnd.ms-excel',
+	'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+	'application/vnd.ms-powerpoint',
+	'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+	'application/msword',
+	'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+)
+TEXTS_ARRAY=('text/csv',
+	'text/css',
+	'text/javascript'
+)
 
-xdg-mime default $IMAGEVIEWER image/jpeg
-xdg-mime default $IMAGEVIEWER image/pjpeg
-xdg-mime default $IMAGEVIEWER image/png
-xdg-mime default $IMAGEVIEWER image/gif
-xdg-mime default $IMAGEVIEWER image/webp
-xdg-mime default $IMAGEVIEWER image/*
-
-xdg-mime default $VIDEOPLAYER video/mp4
-xdg-mime default $VIDEOPLAYER video/mkv
-xdg-mime default $VIDEOPLAYER video/mpeg
-xdg-mime default $VIDEOPLAYER video/avi
-xdg-mime default $VIDEOPLAYER video/x-msvideo
-xdg-mime default $VIDEOPLAYER video/webm
-xdg-mime default $VIDEOPLAYER video/*
-
-xdg-mime default $VIDEOPLAYER application/ogg
-xdg-mime default $VIDEOPLAYER audio/*
+# Loop through each array and set default application for the mimetypes
+for m in ${BROWSER_ARRAY[@]}; do
+	xdg-mime default "${BROWSER}.desktop" $m
+done
+for m in ${IMAGES_ARRAY[@]}; do
+	xdg-mime default $IMAGEVIEWER $m
+done
+for m in ${VIDEOS_ARRAY[@]}; do
+	xdg-mime default $VIDEOPLAYER $m
+done
+for m in ${DOCUMENTS_ARRAY[@]}; do
+	xdg-mime default $DOCREADER $m
+done
+for m in ${TEXTS_ARRAY[@]}; do
+	xdg-mime default $TEXTREADER $m
+done
 
 xdg-mime default $EBOOKREADER application/epub+zip
 
-xdg-mime default $DOCREADER application/pdf
-xdg-mime default $DOCREADER  application/postscript
-# Djvu files open with Zathura (needs zathura-djvu package)
-xdg-mime default $DOCREADER image/vnd.djvu
+exit 0
