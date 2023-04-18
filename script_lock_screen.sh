af28e1e9e7b3947a3f1a2c2fe35421fae7750c4a
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2023-04-18 20:29:47 +0200

    restored previous version of script_lock_screen.sh

--- script_lock_screen.sh
+++ script_lock_screen.sh
@@ -1,44 +1,38 @@
 #!/bin/sh
 
-if [ ! -z $WAYLAND_DISPLAY ]; then
-	# We are under Wayland, use swaylock
-	swaylock -i ~/documents/Images/linuxmasterrace-archlinux-blue.png
-else
-	# We are under X11, use i3lock
-	B='#00000000'  # blank
-	C='#ffffff22'  # clear ish
-	D='#1cd2ee'  # default
-	T='#fffefe'  # text
-	W='#fff0021'  # wrong
-	V='#1fc95a'  # verifying
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#1cd2ee'  # default
+T='#fffefe'  # text
+W='#fff0021'  # wrong
+V='#1fc95a'  # verifying
 
-	i3lock \
-	--blur 7 \
-	--bar-indicator \
-	--bar-pos y+h \
-	--bar-direction 1 \
-	--bar-max-height 50 \
-	--bar-base-width 50 \
-	--bar-color 1cd2ee \
-	--keyhl-color fffefe \
-	--bar-periodic-step 50 \
-	--bar-step 50 \
-	--redraw-thread \
-	\
-	--clock \
-	--force-clock \
-	--time-pos x+5:y+h-80 \
-	--time-color 3e999f \
-	--date-pos tx:ty+15 \
-	--date-color 3e999f \
-	--date-align 1 \
-	--time-align 1 \
-	--ringver-color ff0021 \
-	--ringwrong-color ff0021 \
-	--status-pos x+5:y+h-16 \
-	--verif-align 1 \
-	--wrong-align 1 \
-	--verif-color ffffffff \
-	--wrong-color ffffffff \
-	--modif-pos -50:-50
-fi
+i3lock \
+--blur 7 \
+--bar-indicator \
+--bar-pos y+h \
+--bar-direction 1 \
+--bar-max-height 50 \
+--bar-base-width 50 \
+--bar-color 1cd2ee \
+--keyhl-color fffefe \
+--bar-periodic-step 50 \
+--bar-step 50 \
+--redraw-thread \
+\
+--clock \
+--force-clock \
+--time-pos x+5:y+h-80 \
+--time-color 3e999f \
+--date-pos tx:ty+15 \
+--date-color 3e999f \
+--date-align 1 \
+--time-align 1 \
+--ringver-color ff0021 \
+--ringwrong-color ff0021 \
+--status-pos x+5:y+h-16 \
+--verif-align 1 \
+--wrong-align 1 \
+--verif-color ffffffff \
+--wrong-color ffffffff \
+--modif-pos -50:-50
